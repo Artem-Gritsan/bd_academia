b3b13ff39d7d61f0d7f56949226cfaf85f0ffa81
Artem-Gritsan <gritsanart.ag@gmail.com>
2024-09-24 17:38:35 +0300

    Edit serializers and models

--- db_academy/org_information/models.py
+++ db_academy/org_information/models.py
@@ -2,15 +2,28 @@
 from django.db import models
 
 
+# class Organizations(models.Model):
+#     org_name = models.TextField(primary_key=True)
+#     org_type = models.TextField()
+#     web_site = models.TextField()
+#     director = models.TextField()
+#     phone_number = models.TextField()
+#     org_structure = models.TextField()
+#     org_id = models.IntegerField()
+
+#     class Meta:
+#         managed = False
+#         db_table = 'organizations'
+
+
 class Organizations(models.Model):
-    org_name = models.TextField(primary_key=True)
-    org_type = models.TextField()
-    web_site = models.TextField()
-    director = models.TextField()
-    phone_number = models.TextField()
-    org_structure = models.TextField()
-    org_id = models.IntegerField()
+    org_name = models.CharField(max_length=200, unique=True)
+    org_type = models.CharField(max_length=200)
+    website = models.CharField(max_length=200, null=True)
+    director = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=50, null=True)
+    org_structure = models.CharField(max_length=100)
+    parent = models.ForeignKey('self', on_delete=models.DO_NOTHING, null=True)
 
     class Meta:
-        managed = False
-        db_table = 'organizations'
+        db_table = 'organizations_2'
--- db_academy/org_information/serializers.py
+++ db_academy/org_information/serializers.py
@@ -6,3 +6,13 @@ class OrgDataSerializer(serializers.ModelSerializer):
     class Meta:
         model =  Organizations
         fields = '__all__'
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        if instance.parent:
+            representation['parent'] = {
+                'id' : instance.parent.id,
+                'org_name' : instance.parent.org_name,
+                'org_type' : instance.parent.org_type
+            }
+        return representation
